96849d16ef6162e25c901fd889002bfdf4937d8a
Jeremy Hale <jeremy.hale@gmail.com>
2018-11-14 16:57:56 -0800

    Rever launch script back to Udacity workspace compat

--- scripts/rtab_run.bash
+++ scripts/rtab_run.bash
@@ -24,23 +24,23 @@ read -p 'Enter target world destination or enter for default: ' input_choice
 if [ “$input_choice” = “” ]
 then
  echo "Using default world"
- x-terminal-emulator -e roslaunch slam_project world.launch world_file:=kitchen_dining 2>/dev/null
+ x-terminal-emulator -x roslaunch slam_project world.launch world_file:=kitchen_dining 2>/dev/null &
 
 else
  echo "Using ${input_choice}.world"
- x-terminal-emulator -e roslaunch slam_project world.launch world_file:=${input_choice} 2>/dev/null
+ x-terminal-emulator -x roslaunch slam_project world.launch world_file:=${input_choice} 2>/dev/null &
 fi
 
 sleep 3 &&
 
-x-terminal-emulator -e roslaunch slam_project teleop.launch 2>/dev/null
+x-terminal-emulator -x roslaunch slam_project teleop.launch 2>/dev/null &
 
 sleep 3 &&
 
 echo ' '
 read -p 'Press any key to continue to mapping... ' -n1 -s
 
-x-terminal-emulator -e roslaunch slam_project mapping.launch simulation:=true 2>/dev/null
+x-terminal-emulator -x roslaunch slam_project mapping.launch simulation:=true 2>/dev/null &
 sleep 3 &&
 x-terminal-emulator -e roslaunch slam_project rviz.launch 2>/dev/null
 
